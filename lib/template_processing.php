157c768115f2eeed732497e1d17ce96cbcfb6199
Andrew Horsman <minirobotics@gmail.com>
2010-11-30 22:54:55 -0500

    Refactored, moved functions that don't rely on instance variables into a TextHelper class.  Added support for template variables (&&&title&&&).

--- lib/template_processing.php
+++ lib/template_processing.php
@@ -8,9 +8,9 @@
   require_once(dirname(__FILE__) . '/configuration.php');
 
   /*
-   * Processes a template file with partials and items from DynamicTextProcessing.
+   * Processes a template file with partials and variables.
    */ 
-  class TemplateProcessing extends DynamicTextProcessing {
+  class TemplateProcessing extends TextHelper {
 
     private $templateContents = "";
 
@@ -20,6 +20,7 @@
     public function __construct($templateName) {
       $this->getTemplateContents($templateName); 
       $this->filterForPartials();
+      $this->filterTemplateVariables();
     }
     
     /*
@@ -33,9 +34,9 @@
      * Gets the contents of a template file.
      */
     private function getTemplateContents($templateName) {
-      $templateFilePath = $this->templateFilePath($templateName);
+      $templateFilePath = parent::templateFilePath($templateName);
       if (!file_exists($templateFilePath))
-        $this->error("Template does not exist.  $templateFilePath");   
+        parent::error("Template does not exist.  $templateFilePath");   
         
       $this->templateContents = file_get_contents($templateFilePath);
     }
@@ -44,33 +45,13 @@
      * Gets the contents of a partial file via it's partial name (with <<<>>>).
      */
     private function getPartialContents($partialName) {
-      $partialFilePath = $this->partialFilePath($this->getPartialName($partialName));
+      $partialFilePath = parent::partialFilePath(parent::getPartialName($partialName));
       if (!file_exists($partialFilePath)) 
         return "";
         
       return file_get_contents($partialFilePath);
     } 
 
-    /*
-     * Returns the full file path of a template name.
-     */
-    private function templateFilePath($templateName) {
-      return Configuration::getValue('template_filepath_root') . 
-             '/' . 
-             $templateName . 
-             Configuration::getValue('template_file_extension');
-    }
-    
-    /*
-     * Returns the full file path of a partial name.
-     */
-    private function partialFilePath($partialName) {
-      return Configuration::getValue('template_filepath_root') .
-             '/' .
-             $partialName .
-             Configuration::getValue('partial_file_extension');
-    }
-    
     /*
      * Searches the template contents for partial syntax (<<<partial_name>>>) 
      * and replaces each match with the contents of the partial.
@@ -89,30 +70,72 @@
       $this->templateContents = str_replace($search, $replace, $this->templateContents);
     }
     
+    private function filterTemplateVariables() {
+      if (preg_match_all("/&&&.*?&&&/m", $this->templateContents, $matches) !== false) {
+        foreach ($matches[0] as $match) {
+        
+          switch (parent::getVariableName($match)) {
+            case "title":
+              $this->replaceTemplateContents($match, Configuration::getValue('base_title'));
+              break;
+            default:
+              $this->replaceTemplateContents($match, "Processing error.");
+          }
+          
+        }
+      }
+    }
+
+  }
+  
+  /*
+   * Various text processing functions that don't rely on an instance state.
+   */ 
+  class TextHelper {
+  
+    /*
+     * Removes the variable syntax from a matched variable name.
+     */
+    protected function getVariableName($variable) {
+      return str_replace("&&&", '', $variable);
+    }
+    
     /*
      * Removes the partial syntax from a matched partial name.
      */
-    private function getPartialName($partial) {
+    protected function getPartialName($partial) {
       return str_replace(array("<<<", ">>>"), '', $partial);
     } 
     
     /*
+     * Returns the full file path of a template name.
+     */
+    protected function templateFilePath($templateName) {
+      return Configuration::getValue('template_filepath_root') . 
+             '/' . 
+             $templateName . 
+             Configuration::getValue('template_file_extension');
+    }
+    
+    /*
+     * Returns the full file path of a partial name.
+     */
+    protected function partialFilePath($partialName) {
+      return Configuration::getValue('template_filepath_root') .
+             '/' .
+             $partialName .
+             Configuration::getValue('partial_file_extension');
+    }
+    
+    /*
      * Log an error on the server and kill the page with an error message.
      */
-    private function error($message) {
+    protected function error($message) {
       error_log($message);
       die($message);
-    }
-
+    } 
+  
   }
-
-  /*
-   * Template variables that need to be parsed and do not come from a static 
-   * partial.
-   */
-  class DynamicTextProcessing {
-
-  }
-
+  
 ?>  
 
