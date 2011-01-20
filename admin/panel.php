6d4dfe011c50cc9b3666615c917354af357433c0
Andrew Horsman <minirobotics@gmail.com>
2011-01-19 19:52:52 -0500

    Changed photo.php to photos.php.

--- admin/panel.php
+++ admin/panel.php
@@ -18,7 +18,7 @@
 	function displayConsole() {
 		?>
 			<ul>
-				<li><a href="photo.php" title="Upload and Edit Photos">Add and Edit Photos</a></li>
+				<li><a href="photos.php" title="Upload and Edit Photos">Add and Edit Photos</a></li>
 				<li><a href="videos.php" title="Add and Edit YouTube Videos">Add and Edit YouTube Videos</a></li>
 				<li><a href="panel.php?action=logout" title="Logout">Logout</a></li>
 			</ul>
